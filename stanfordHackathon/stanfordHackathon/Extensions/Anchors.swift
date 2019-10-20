f87a39e7b4741dfcdc1d706718c3922d27009e1a
Shaina Mae Mainar <shainamainar@gmail.com>
2019-10-19 20:35:51 -0700

    Add files via upload

---
+++ stanfordHackathon/stanfordHackathon/Extensions/Anchors.swift
@@ -0,0 +1,90 @@
+//
+//  Anchors.swift
+//  stanfordHackathon
+//
+//  Created by Chandler Long on 10/19/19.
+//  Copyright © 2019 C-LongDev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func centerX(centerX: NSLayoutXAxisAnchor?) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let centerX = centerX {
+            self.centerXAnchor.constraint(equalTo: centerX).isActive = true
+        }
+    }
+    
+    func centerY(centerY: NSLayoutYAxisAnchor?) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let centerY = centerY {
+            self.centerYAnchor.constraint(equalTo: centerY).isActive = true
+        }
+    }
+    
+    func setAnchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?,
+                   bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,
+                   paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat,
+                   paddingRight: CGFloat, width: CGFloat = 0, height: CGFloat = 0) {
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if width != 0 {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.topAnchor
+        }
+        return topAnchor
+    }
+    
+    var safeLeftAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.leftAnchor
+        }
+        return leftAnchor
+    }
+    
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.bottomAnchor
+        }
+        return bottomAnchor
+    }
+    
+    var safeRightAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.rightAnchor
+        }
+        return rightAnchor
+    }
+    
+}
